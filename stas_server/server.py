9925f07965269b4669658d92c7b8308a5328c958
Muhd. Danial Danish Roslan <distantwaterstream@gmail.com>
2025-08-23 22:33:39 +0800

    feat: migrate from bottle.py to aiohttp
    chore: remove unused bottle and tornado and add aiohttp

--- stas_server/server.py
+++ stas_server/server.py
@@ -1,9 +1,8 @@
-import json
 import sys
 import logging
-from typing import Callable
+from typing import Awaitable, Callable
 
-from bottle import Bottle, request, response
+from aiohttp import web
 
 import stas_server.config as config
 from stas_server.util import (
@@ -12,23 +11,8 @@ from stas_server.util import (
     recombine_split_list,
 )
 
-log_server_bottle = logging.getLogger("Server")
+log_server = logging.getLogger("Server")
 log_translation = logging.getLogger("Translation")
-
-app = Bottle()
-
-
-def route(**kwargs):
-    def decorator(callback):
-        kwargs["callback"] = callback
-        app.route(**kwargs)
-
-        kwargs["method"] = "OPTIONS"
-        kwargs["callback"] = lambda: {}
-        app.route(**kwargs)
-        return callback
-
-    return decorator
 
 
 def run_server(
@@ -36,62 +20,74 @@ def run_server(
     translate_batch_func: Callable[[list[str]], list[str]],
     check_func: Callable[[str], bool],
 ):
-    @app.hook("after_request")
-    def enable_cors():
+    @web.middleware
+    async def cors_middleware(
+        request: web.Request,
+        handler: Callable[[web.Request], Awaitable[web.StreamResponse]],
+    ):
         """
         You need to add some headers to each request.
         Don't use the wildcard '*' for Access-Control-Allow-Origin in production.
         PNA (https://developer.chrome.com/blog/private-network-access-preflight)
         """
-        response.headers["Access-Control-Allow-Origin"] = "*"
-        response.headers["Access-Control-Allow-Methods"] = (
-            "PUT, GET, POST, DELETE, OPTIONS"
+        response = await handler(request)
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        response.headers.add(
+            "Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS"
         )
-        response.headers["Access-Control-Allow-Headers"] = (
-            "Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token"
+        response.headers.add(
+            "Access-Control-Allow-Headers",
+            "Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token",
         )
-        response.headers["Access-Control-Allow-Private-Network"] = "true"
+        response.headers.add("Access-Control-Allow-Private-Network", "true")
+        return response
 
-    @route(path="/", method="POST")
-    def translate_text():
-        data = request.json
+    async def translate_text(request: web.Request):
+        data = await request.json()
         message = data.get("message")
 
         if message == "translate sentences":
             content = data.get("content")
-            log_server_bottle.info("Receive content from server")
+            log_server.info("Receive content from server")
 
             content = process_raw_string(content)
             if check_func(content):
                 log_translation.info("Text is Japanese.")
                 result = translate_func(content)
-                return json.dumps(result)
+                return web.json_response(result)
             else:
                 log_translation.info("Text is not Japanese.")
-                return json.dumps(content)
+                return web.json_response(content)
 
         if message == "translate batch":
             content = data.get("batch")
-            log_server_bottle.info("Receive content batch from server")
+            log_server.info("Receive content batch from server")
 
             if len(content) == 0:
                 log_translation.info("No text in the batch.")
-                return json.dumps(content)
+                return web.json_response(content)
 
             sp, le, ix = split_list_by_condition(content, check_func)
             sp = list(map(process_raw_string, sp))
             if len(sp) == 0:
                 log_translation.info("All text in the batch are not Japanese.")
-                return json.dumps(content)
+                return web.json_response(content)
             else:
                 log_translation.info("Japanese text found in the batch.")
                 result = translate_batch_func(sp)
                 final_result = recombine_split_list(result, le, ix)
-                return json.dumps(final_result)
+                return web.json_response(final_result)
 
         if message == "close server":
-            app.close()
+            app.shutdown()
             sys.exit(0)
 
-    log_server_bottle.info(f"Listening on http://localhost:{config.port}")
-    app.run(server="tornado", host="0.0.0.0", port=config.port, debug=False, quiet=True)
+    app = web.Application(middlewares=[cors_middleware])
+    app.add_routes(
+        [
+            web.post("/", translate_text),
+            web.options("/", lambda req: web.Response()),
+        ]
+    )
+    log_server.info(f"Listening on http://localhost:{config.port}")
+    web.run_app(app, port=config.port, access_log=log_server, print=None)
